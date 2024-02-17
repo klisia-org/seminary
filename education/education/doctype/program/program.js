5214bf1f7957bb83ff1b95fa808c8dd5e36163ea
drmurilomelo <67798777+drmurilomelo@users.noreply.github.com>
2024-02-17 08:00:04 -0600

    program and pgmcourse

--- education/education/doctype/program/program.js
+++ education/education/doctype/program/program.js
@@ -12,3 +12,24 @@ frappe.ui.form.on('Program Course', {
 		};
 	}
 });
+
+// Update form based on program_type: if program_type = 'Time-based' show terms_complete and hide credits_complete; if program_type = 'Credit-based' show credits_complete and hide terms_complete
+frappe.ui.form.on('Program', 'program_type', function(frm) {
+	if (frm.doc.program_type == 'Time-based') {
+		frm.toggle_display('terms_complete', true);
+		frm.toggle_display('credits_complete', false);
+	} else if (frm.doc.program_type == 'Credit-based') {
+		frm.toggle_display('credits_complete', true);
+		frm.toggle_display('terms_complete', true);
+	}
+});
+
+// Update label of terms_complete based on program_type; if program_type = 'Time-based' label = 'Terms to Complete (Will be used to auto-enroll passing students)'; if program_type = 'Credit-based' label terms_complete as 'Suggested # terms to complete (will only be used for statistical purposes)' and label credits_complete = 'Minimum # of Credits to Graduate'
+frappe.ui.form.on('Program', 'program_type', function(frm) {
+	if (frm.doc.program_type == 'Time-based') {
+		frm.set_df_property('terms_complete', 'label', 'Terms to Complete (Will be used to auto-enroll passing students)');
+		} else if (frm.doc.program_type == 'Credit-based') {
+		frm.set_df_property('terms_complete', 'label', 'Suggested # terms to complete (will only be used for statistical purposes)');
+		frm.set_df_property('credits_complete', 'label', 'Minimum # of Credits to Graduate');
+	}
+});
