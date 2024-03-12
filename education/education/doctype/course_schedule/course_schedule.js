b16a714435524b1676e0f7b756ad80c9e6f4add0
drmurilomelo <67798777+drmurilomelo@users.noreply.github.com>
2024-03-12 08:57:21 -0500

    Update course_schedule.js

--- education/education/doctype/course_schedule/course_schedule.js
+++ education/education/doctype/course_schedule/course_schedule.js
@@ -26,4 +26,19 @@ frappe.ui.form.on("Course Schedule", {
 		})
 
 	}
-});
+}),
+
+onsave; (frm) => {
+	frappe.call({
+		method: 'education.education.course_schedule.get_meeting_dates',
+		args: {
+			'docname': frm.doc.name
+},
+	callback: function(r) {
+		if (r.message) {
+			frm.set_value('meeting_dates', r.message);
+		}
+		else
+			return;
+}
+})};
