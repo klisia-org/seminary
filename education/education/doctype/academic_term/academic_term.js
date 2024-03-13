4943cbaff19fb387d234d9e7c359d3a0a7659222
drmurilomelo <67798777+drmurilomelo@users.noreply.github.com>
2024-03-13 16:36:52 -0500

    Update academic_term.js

--- education/education/doctype/academic_term/academic_term.js
+++ education/education/doctype/academic_term/academic_term.js
@@ -1,13 +1,5 @@
 // Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.listview_settings['Academic Term'].refresh = function(listview) {
-	listview.page.add_menu_item(__("Roll Term"));
-	listview.page.add_action_item(__('Roll Term'), function() {
-		
-		frappe.call("education.education.education.api.set_iscurrent_acterm")
-				.then(r =>  {
-				console.log(r)
-				})});};
 frappe.ui.form.on('Academic Term', {
 });
