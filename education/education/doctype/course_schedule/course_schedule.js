1ba925bb279941862ea7f7633222706f57a73567
drmurilomelo <67798777+drmurilomelo@users.noreply.github.com>
2024-03-15 14:19:29 -0500

    Update course_schedule.js

--- education/education/doctype/course_schedule/course_schedule.js
+++ education/education/doctype/course_schedule/course_schedule.js
@@ -2,9 +2,10 @@ frappe.ui.form.on("Course Schedule", {
 	refresh: function(frm) {
 		frm.add_custom_button(__('Add Meeting Dates'), function()  {
 			if(!frm.cs_meetinfo) {
+				frm.save();
 				frm.call('save_dates');
 				frappe.msgprint('Meeting dates added');
-				frm.refresh_field('cs_meetinfo');
+				frm.refresh();
 		
 				} else { 
 					frappe.msgprint('No meeting dates found')}})
