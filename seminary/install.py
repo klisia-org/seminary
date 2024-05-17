8296aa5c9d15773b7ec3b03db61eeeb5fdbd12d5
drmurilomelo <67798777+drmurilomelo@users.noreply.github.com>
2024-05-17 07:57:48 -0500

    icon and cleanup

--- seminary/install.py
+++ seminary/install.py
@@ -1,6 +1,8 @@
 import frappe
 #from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 from frappe.desk.page.setup_wizard.setup_wizard import make_records
+import os
+import re
 
 
 # TODO: create uninstall file and remove all the custom fields, roles, assessment groups, fixtures, etc
@@ -15,6 +17,7 @@ def after_install():
 	create_registrar_role()
 	create_instructor_role()
 	get_custom_fields()
+	icon_install()
 	
 
 def setup_fixtures():
@@ -69,3 +72,23 @@ def get_custom_fields():
 				"insert_after": "student_info_section",
 			},
 				],}
+
+def icon_install():
+    """
+    Append the SVG symbol file to the sites/assets/frappe/icons/timeless/icons.svg file. Relative path is seminary\public\icons\bible.svg 
+    """
+    src_path = os.path.join(frappe.get_app_path('seminary'), 'public', 'icons', 'bible.svg')
+    dest_path = os.path.join(os.path.dirname(frappe.get_site_path()), 'assets', 'frappe', 'icons', 'timeless', 'icons.svg')
+
+    with open(dest_path, 'r+') as f:
+        content = f.read()
+
+        # Find the closing </svg> tag.
+        m = re.search(r'</svg>', content)
+        if m:
+            # Append the SVG symbol file to the content.
+            content = content[:m.start()] + open(src_path, 'r').read() + content[m.start():]
+
+        f.seek(0)
+        f.write(content)
+        f.truncate()
