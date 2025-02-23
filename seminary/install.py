4ecb3b8c3d248adc1728dd12d318adedac4e37f8
drmurilomelo <drmrmelo@MU-GamingCP1>
2025-02-23 14:55:40 -0600

    Optimized install

--- seminary/install.py
+++ seminary/install.py
@@ -17,6 +17,9 @@ def after_install():
 	create_registrar_role()
 	create_instructor_role()
 	get_custom_fields()
+	update_trigger_fee_events()
+	delete_genders()
+	update_grading_scale()
 	
 	
 
@@ -32,9 +35,11 @@ def setup_fixtures():
 		{"doctype": "Customer Group", "customer_group_name": "Denomination"},
 		{"doctype": "Customer Group", "customer_group_name": "Seminary"},
 		{"doctype": "Customer Group", "customer_group_name": "Para-church Organization"},
-		# Trigger Fee Events
-		{"doctype": "Trigger Fee Event", "trigger": "Course Enrollment", "Active": "1"},
-		{"doctype": "Trigger Fee Event", "trigger": "New Academic Term", "Active": "1"},
+		# Stock Item Records
+		{"doctype": "Item", "item_code": "Credit hour", "item_name": "Credit hour", "item_group": "Tuition", "stock_uom": "Unit", "is_sales_item": 1},
+		{"doctype": "Item", "item_code": "Program Admission", "item_name": "Program Admission", "item_group": "Tuition", "stock_uom": "Unit", "is_sales_item": 1},
+		{"doctype": "Item", "item_code": "Donation for Scholarship", "item_name": "Donation for Scholarship", "item_group": "Tuition", "stock_uom": "Unit", "is_sales_item": 1},
+		
 	
 		]
 	make_records(records)
@@ -75,5 +80,50 @@ def get_custom_fields():
 				"insert_after": "student_info_section",
 			},
 				],}
+def update_trigger_fee_events():
+		for trigger in (
+			_("Program Enrollment"),
+			_("Course Enrollment"),
+			_("New Academic Year"),
+			_("New Academic Term"),
+			_("Monthly"),
+		):
+			doc = frappe.new_doc("Trigger Fee Event")
+			doc.trigger = trigger
+			doc.active = 1
+			doc.insert(ignore_permissions=True, ignore_if_duplicate=True)
+
+def delete_genders():
+	# Delete auto inserted genders 
+	frappe.db.sql("DELETE FROM `tabGender' WHERE name NOT IN ('Male', 'Female')")
+
+def update_grading_scale():
+	# Create Default Grading Scale and populate child table Grading Scale Interval
+	if not frappe.db.exists("Grading Scale", "Default"):
+		doc = frappe.get_doc({
+			"doctype": "Grading Scale",
+			"grading_scale_name": "Default Grading Scale",
+			"maxnumgrade": 100,
+			"grscale_type": "Points",
+			
+			"interval": [
+				{"grade_code": "A+", "threshold": 98, "grade_description": "A+", "grade_pass": "Pass"},
+				{"grade_code": "A", "threshold": 96, "grade_description": "A", "grade_pass": "Pass"},
+				{"grade_code": "A-", "threshold": 94, "grade_description": "A-", "grade_pass": "Pass"},
+				{"grade_code": "B+", "threshold": 92, "grade_description": "B+", "grade_pass": "Pass"},
+				{"grade_code": "B", "threshold": 89, "grade_description": "B", "grade_pass": "Pass"},
+				{"grade_code": "B-", "threshold": 86, "grade_description": "B-", "grade_pass": "Pass"},
+				{"grade_code": "C+", "threshold": 83, "grade_description": "C+", "grade_pass": "Pass"},
+				{"grade_code": "C", "threshold": 80, "grade_description": "C", "grade_pass": "Pass"},
+				{"grade_code": "C-", "threshold": 77, "grade_description": "C-", "grade_pass": "Pass"},
+				{"grade_code": "D+", "threshold": 74, "grade_description": "D+", "grade_pass": "Pass"},
+				{"grade_code": "D", "threshold": 72, "grade_description": "D", "grade_pass": "Pass"},
+				{"grade_code": "D-", "threshold": 69, "grade_description": "D-", "grade_pass": "Pass"},
+				{"grade_code": "F", "threshold": 0, "grade_description": "F", "grade_pass": "Fail"},
+			]
+		})
+		doc.insert(ignore_permissions=True)
+		frappe.db.commit()
+
 
 
