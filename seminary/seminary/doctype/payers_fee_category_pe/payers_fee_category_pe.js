217c2b783f39812f31116d3dafb0a4eb755bb6e3
drmurilomelo <67798777+drmurilomelo@users.noreply.github.com>
2025-02-28 07:24:25 -0600

    Update payers_fee_category_pe.js

--- seminary/seminary/doctype/payers_fee_category_pe/payers_fee_category_pe.js
+++ seminary/seminary/doctype/payers_fee_category_pe/payers_fee_category_pe.js
@@ -9,8 +9,10 @@ frappe.ui.form.on("Payers Fee Category PE", {
                 frm.call('get_inv_data_pe')
                     .then(r => {
                         frm.set_value("si_created", 1);
-                        frm.save();
-                        frm.refresh();
+                        frm.save()
+                            .then(() => {
+                                frm.refresh();
+                            });
                     })
                     .catch(e => {
                         frappe.msgprint("Error creating Sales Invoice(s)!");
@@ -18,18 +20,6 @@ frappe.ui.form.on("Payers Fee Category PE", {
             }).css({"color":"white", "background": "#0d3049", "font-weight": "700", "border-radius": "5px", "padding": "5px 10px", "margin-right": "10px"}); 
             console.log("Sales Invoice button added");
         }
-
-        frm.add_custom_button("Add Scholarship", function() {
-            console.log("Calling add_scholarship");
-            frm.call('add_scholarship')
-                .then(r => {
-                    console.log("Add scholarship called");
-                })
-                .catch(e => {
-                    console.log("Error calling add_scholarship:", e);
-                });
-        }).css({"color":"white", "background": "#0d3049", "font-weight": "700", "border-radius": "5px", "padding": "5px 10px", "margin-right": "10px"});
-        console.log("Add Scholarship button added");
     },      
 
     after_save(frm) {
@@ -44,10 +34,15 @@ frappe.ui.form.on("Payers Fee Category PE", {
             .catch(e => {
                 console.log("Error calling check_percentages:", e);
             });
-        console.log("Calling seminary.seminary.api.add_scholarship with doc name:", frm.doc.name);
-        frm.call('add_scholarship', frm.doc.name)
+
+        console.log("Calling add_scholarship automatically after save");
+        frm.call('add_scholarship')
             .then(r => {
                 console.log("add_scholarship called successfully");
+                frm.save()
+                    .then(() => {
+                        frm.reload_doc(); // Reload the form after saving
+                    });
             })
             .catch(e => {
                 console.log("Error calling add_scholarship:", e);
@@ -60,14 +55,6 @@ frappe.ui.form.on("Payers Fee Category PE", {
             return {
                 query: "seminary.seminary.api.get_scholarships"
             };      
-            
         });
-                  
-           
-        
-                    
     }
-               
-      
-    
 });
