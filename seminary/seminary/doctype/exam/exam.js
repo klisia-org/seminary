ac5f3c0b02aa0b544788524c81621e70f80d7a5a
drmurilomelo <67798777+drmurilomelo@users.noreply.github.com>
2025-03-10 14:06:17 -0500

    Exam

---
+++ seminary/seminary/doctype/exam/exam.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Klisia / SeminaryERP and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Exam", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ seminary/seminary/doctype/open_question/open_question.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Klisia / SeminaryERP and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Open Question", {
+// 	refresh(frm) {
+
+// 	},
+// });
