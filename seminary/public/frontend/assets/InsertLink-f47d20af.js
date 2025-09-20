5a19fd9491ee7726b3c091df3f489660b629f5d8
drmurilomelo <drmrmelo@MU-GamingCP1>
2025-09-20 17:38:10 -0500

    ui update

--- seminary/public/frontend/assets/InsertLink-f47d20af.js
+++
@@ -1,2 +0,0 @@
-import{K as t,N as s,Q as a,R as i}from"./index-99ce2d99.js";const c=t({__name:"InsertLink",props:{editor:{}},setup(o){const e=o;function n(){e.editor.commands.openLinkEditor()}return(r,p)=>s(r.$slots,"default",a(i({onClick:n})))}});export{c as default};
-//# sourceMappingURL=InsertLink-f47d20af.js.map
---
+++ seminary/public/frontend/assets/MissingData-mJ_s9lGQ.js
@@ -0,0 +1,2 @@
+import{d as t,o as s,f as a,t as o}from"./index-BxfdauWF.js";const c={class:"flex items-center justify-center h-[250px]"},n={class:"text-gray-500"},p={__name:"MissingData",props:{message:{type:String,default:"No data found"}},setup(e){return(r,i)=>(s(),t("div",c,[a("p",n,o(e.message),1)]))}};export{p as _};
+//# sourceMappingURL=MissingData-mJ_s9lGQ.js.map
