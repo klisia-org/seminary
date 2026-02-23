402fa5f6e6632e8128667768df4c4cdc26353229
Murilo R. Melo <drmurilomelo@github.com>
2026-02-23 11:56:12 -0600

    Upgraded dependencies for security issues

---
+++ seminary/public/frontend/assets/settings-B2Pofed8.js
@@ -0,0 +1,2 @@
+import{bB as o,l as s}from"./vendor-CVNNLuVR.js";import"./frappe-ui-BF0Q74Zp.js";import{s as n}from"./index-C5UWiriW.js";const f=o("settings",()=>{const{isLoggedIn:i}=n(),t=s(!1),e=s(null);return{isSettingsOpen:t,activeTab:e}});export{f as u};
+//# sourceMappingURL=settings-B2Pofed8.js.map
