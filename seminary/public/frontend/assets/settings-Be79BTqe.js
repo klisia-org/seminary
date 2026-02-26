a269b89ced1030db66929ac10df9641085783c67
Murilo R. Melo <drmurilomelo@github.com>
2026-02-26 08:07:01 -0600

    Improvements in Student Groups, and used course filters on Assessment Plugin data flow

--- seminary/public/frontend/assets/settings-Be79BTqe.js
+++
@@ -1,2 +0,0 @@
-import{bV as n,x as s}from"./vendor-BEGlqOqg.js";import{s as o}from"./index-BXigJxq3.js";const c=n("settings",()=>{const{isLoggedIn:i}=o(),t=s(!1),e=s(null);return{isSettingsOpen:t,activeTab:e}});export{c as u};
-//# sourceMappingURL=settings-Be79BTqe.js.map
---
+++ seminary/public/frontend/assets/settings-C6t9qJhl.js
@@ -0,0 +1,2 @@
+import{bV as n,x as s}from"./vendor-Ba-V_5WO.js";import{s as o}from"./index-DJ9Dx9ZW.js";const c=n("settings",()=>{const{isLoggedIn:i}=o(),t=s(!1),e=s(null);return{isSettingsOpen:t,activeTab:e}});export{c as u};
+//# sourceMappingURL=settings-C6t9qJhl.js.map
